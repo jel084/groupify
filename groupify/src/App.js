279c8f7dc48ba83750c32482bdadda3bfe9b6078
jel084 <130106981+jel084@users.noreply.github.com>
2024-09-04 23:30:40 -0700

    set up screen router

--- groupify/src/App.js
+++ groupify/src/App.js
@@ -1,27 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './screens/SignIn';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ groupify/src/screens/startingScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const startingScreen = () => {
+    return (
+      <div className="center">
+        <header className="bg-[#ffffff] min-h-screen flex flex-col items-center justify-center text-[calc(10px_+_2vmin)] text-[black]">
+          <h1 className="text-3xl font-bold underline">
+            Hello j99999!
+          </h1>
+        </header>
+      </div>
+    );
+  }
+  
+  export default startingScreen;
